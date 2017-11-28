8dc8c917596535e99eea429b8984070d68122ff9
Thomas BAILLY-SALINS <tbailly-@e3r12p20.42.fr>
2017-11-28 21:14:42 +0100

    Now the basics are there, but code does not manage very little buf and very long buf with long txt (and so many more bugs to come

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbailly- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 21:16:48 by tbailly-          #+#    #+#             */
-/*   Updated: 2017/11/27 22:25:36 by tbailly-         ###   ########.fr       */
+/*   Updated: 2017/11/28 21:12:53 by tbailly-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	**ft_read_file(int fd, char **line, char **storage_pt)
 	if ((nbchr = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[nbchr] = '\0';
-		printf("buf: '%s' et nbchr=%i\n", buf, nbchr);
+		//'printf("buf: '%s' et nbchr=%i\n", buf, nbchr);
 		while(buf[i] != '\0')
 		{
 			if (buf[i] == '\n')
@@ -33,80 +33,112 @@ char	**ft_read_file(int fd, char **line, char **storage_pt)
 		}
 		if (buf[i] == '\0')
 		{
-			printf("Pas de \\n dans le buffer actuel : '%s'\n", buf);
+			//printf("Pas de \\n dans le buffer actuel : '%s'\n", buf);
 			if (line)
 			{
+				//printf("1 '%s'\n", *line);
 				*line = ft_strjoin(*line, buf);
-				printf("1 '%s'\n", *line);
 			}
 			else
 			{
-				line = (char**)malloc(sizeof(*line));
+				//printf("2 '%s'\n", *line);
+				//line = (char**)malloc(sizeof(*line));
 				*line = ft_strdup(buf);
-				printf("2 '%s'\n", *line);
 			}
-			line = ft_read_file (fd, line, storage_pt);
+			line = ft_read_file(fd, line, storage_pt);
 		}
 		else
 		{
 			//printf("\\n dans le buffer actuel : '%s' a la pos %i\n", buf, i);
-			*line = ft_strnjoin(*line, buf, i);
-			//printf("ooo '%s'\n", *line);
-			*storage_pt = ft_strdup(&(buf[i + 1]));
-			//printf("storage '%s'\n", *storage_pt);
+			if (line)
+			{
+				*line = ft_strnjoin(*line, buf, i);
+				*storage_pt = ft_strdup(&(buf[i + 1]));
+				//printf("3 '%s'\n", *line);
+			}
+			else
+			{
+				//line = (char**)malloc(sizeof(*line));
+				*line = ft_strndup(buf, i);
+				*storage_pt = ft_strdup(&(buf[i + 1]));
+				//printf("4 '%s'\n", *line);
+			}
 		}
 		return (line);
 	}
 	return (NULL);
 }
 
+char	*ft_strpop_front(char *src, int n)
+{
+	char	*ret;
+	int		len;
+	int		i;
+
+	len = ft_strlen(src);
+	//printf("len %i et n\n", len);
+	i = 0;
+	if (!(ret = (char*)malloc(sizeof(*ret) * (len - n))))
+		return (NULL);
+	//printf("n %i et src[n] %c et len %i\n", n, src[n], len);
+	while (n < len)
+	{
+		ret[i] = src[n];
+		n++;
+		i++;
+	}
+	//free(src);
+	//src = NULL;
+	ret[i] = '\0';
+	//printf("stor %s and i%i\n", ret, i);
+	return (ret);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	printf("GETNEXTLINE %i\n", fd);
+	//printf("--------------- GETNEXTLINE\n");
 	
 	static	char	*storage;
 	int				i;
 
 	i = 0;
 	if (!storage)
+		if (!(storage = (char*)malloc(sizeof(*storage))))
+			return (-1);
+	if (!(line = (char**)malloc(sizeof(*line))))
+		return (-1);
+	if (!(*line = (char*)malloc(sizeof(line))))
+		return (-1);
+	
+	while (storage[i] != '\0')
 	{
+		if (storage[i] == '\n')
+			break;
+		i++;
+	}
+	if (storage[i] == '\0')
+	{
+		//printf("Pas de \\n dans storage\n");
+		if (storage[0] != '\0')
+			*line = ft_strdup(storage);
 		line = ft_read_file(fd, line, &storage);
-		printf("Premiere ligne '%s' et reste '%s'\n", *line, storage);
+	}
+	else if (i == 0)
+	{
+		storage = ft_strpop_front(storage, i + 1);
+		//printf("storagetirhgrt %s\n", storage);
+		//return (1);
 	}
 	else
 	{
-		while (storage[i] != '\0')
-		{
-			if (storage[i] == '\n')
-				break;
-			i++;
-		}
-		if (storage[i] == '\0')
-		{
-			printf("Pas de \\n dans storage\n");
-			//*line = ft_strndup(*line, storage, i);
-			//line = ft_read_file(fd, line, &storage);
-		}
-		else
-		{
-			printf("\\n dans storage i=%i\n", i);
-			//*line = ft_strncpy(*line, storage, i);
-			//storage = ft_strcut(storage, 0, i);
-		}
+		//printf("\\n dans storage i=%i\n", i);
+		*line = ft_strndup(storage, i);
+		//printf("storage avant %s et i%i\n", storage, i);
+		storage = ft_strpop_front(storage, i + 1);
+		//printf("storage apres %s\n", storage);
 	}
+	//printf("--------------- LINE %s\n--------------- STORAGE %s\n", *line, storage);
+	printf("LINE %s\n", *line);
 
-
-
-
-	static	int	a;
-	if (!a)
-	{
-		a = 10;
-	}
-	a++;
-
-	line = (char**)malloc(sizeof(*line));
-	line[0] = (char*)malloc(sizeof(line));
-	line[0][0] = 'b';
-	return (a);
+	return (-1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: tbailly- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 21:19:23 by tbailly-          #+#    #+#             */
-/*   Updated: 2017/11/27 20:52:06 by tbailly-         ###   ########.fr       */
+/*   Updated: 2017/11/28 21:12:51 by tbailly-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		GET_NEXT_LINE_H
 # define	GET_NEXT_LINE_H
-# define	BUFF_SIZE 20
+# define	BUFF_SIZE 2000
 
 # include	<unistd.h>
 # include	<stdlib.h>
