1f415909b43653bd685c147622b1567f57339f84
Thomas BAILLY-SALINS <tbailly-@e3r13p8.42.fr>
2017-11-30 19:58:06 +0100

    Almost done, norm is ok, but don't manage multi-fd and some weird bugs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbailly- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 21:16:48 by tbailly-          #+#    #+#             */
-/*   Updated: 2017/11/30 00:03:34 by tbailly-         ###   ########.fr       */
+/*   Updated: 2017/11/30 19:27:32 by tbailly-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,32 @@
 #include "get_next_line.h"
 #include "libft/libft.h"
 
-int		ft_has_newline(char **storage_pt, char *buf, char **line, int i)
+int		ft_cannot_read(int nbchr, char **storage_pt, char **line)
+{
+	int	ret;
+
+	ret = 0;
+	if (nbchr == 0)
+	{
+		if (*storage_pt)
+			ft_memdel((void**)storage_pt);
+		if (*line[0] != '\0')
+			ret = 1;
+	}
+	else
+		ret = -1;
+	return (ret);
+}
+
+int		ft_has_newline(char **st_pt, char *buf, char **line, int i)
 {
 	char	*tmp;
 
 	tmp = *line;
 	if (buf == NULL)
 	{
-		(*storage_pt)[i] = '\0';	
-		*line = ft_strdup(*storage_pt);
+		(*st_pt)[i] = '\0';
+		*line = ft_strdup(*st_pt);
 	}
 	else
 	{
@@ -32,12 +49,12 @@ int		ft_has_newline(char **storage_pt, char *buf, char **line, int i)
 	if (line == NULL)
 		return (-1);
 	free(tmp);
-	tmp = *storage_pt;
+	tmp = *st_pt;
 	if (buf == NULL)
-		*storage_pt = ft_strsub(*storage_pt, i + 1, ft_strlen(&(*storage_pt)[i + 1]));
+		*st_pt = ft_strsub(*st_pt, i + 1, ft_strlen(&(*st_pt)[i + 1]));
 	else
-		*storage_pt = ft_strsub(buf, i + 1, ft_strlen(&buf[i + 1]));
-	if (*storage_pt == NULL)
+		*st_pt = ft_strsub(buf, i + 1, ft_strlen(&buf[i + 1]));
+	if (*st_pt == NULL)
 		return (-1);
 	free(tmp);
 	return (1);
@@ -64,71 +81,52 @@ int		ft_read_file(int fd, char **line, char **storage_pt)
 	char	buf[BUFF_SIZE + 1];
 	int		i;
 	int		ret;
-	
+
 	i = -1;
+	ret = 0;
 	if ((nbchr = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[nbchr] = '\0';
-		//printf("Je lis %s nbchr %i *line %s storage %s\n", buf, nbchr, *line, *storage_pt);
-		while(++i < nbchr)
+		while (++i < nbchr)
 			if (buf[i] == '\n')
-				break;
-		if (i == nbchr)
+				break ;
+		if (buf[i] == '\0' && i == nbchr)
 		{
-			//printf("NO NWL\n");
 			ft_no_newline(storage_pt, buf, line);
 			ret = ft_read_file(fd, line, storage_pt);
 		}
-		else
-		{
-			//printf("NWL\n");
+		else if (buf[i] == '\n')
 			ret = ft_has_newline(storage_pt, buf, line, i);
-		}
-		if (nbchr > 0)
-			return (1);
-		return (ret);
 	}
-	else if (nbchr == 0)
-	{
-		if (*storage_pt)
-		{
-			free(*storage_pt);
-			*storage_pt = NULL;
-		}
-		return (0);
-	}
-	return (-1);
+	else
+		ret = ft_cannot_read(nbchr, storage_pt, line);
+	return (ret);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static	char	*storage;
-	char			*tmp;
 	int				i;
 	int				ret;
 
-	tmp = NULL;
 	i = -1;
+	if (line == NULL)
+		return (-1);
 	if (!storage)
-		if (!(storage = (char*)malloc(sizeof(*storage))))
+		if (!(storage = (char*)ft_memalloc(sizeof(*storage))))
 			return (-1);
 	if (!(*line = (char*)malloc(sizeof(line))))
 		return (-1);
 	while (storage[++i] != '\0')
 		if (storage[i] == '\n')
-			break;
-	//printf("Donc i=%i et stor '%s'\n", i, storage);
-	//printf("len %zu et storage '%s'\n", ft_strlen(storage), storage);
+			break ;
 	if (storage[i] == '\0')
 	{
-		//printf("NO NWL\n");
-		ft_no_newline(&storage, NULL, line);
+		if (ft_no_newline(&storage, NULL, line) == -1)
+			return (-1);
 		ret = ft_read_file(fd, line, &storage);
 	}
 	else
-	{
-		//printf("NWL\n");
 		ret = ft_has_newline(&storage, NULL, line, i);
-	}
 	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: tbailly- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 21:19:23 by tbailly-          #+#    #+#             */
-/*   Updated: 2017/11/30 00:03:43 by tbailly-         ###   ########.fr       */
+/*   Updated: 2017/11/30 19:35:03 by tbailly-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
-# define	BUFF_SIZE 20
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 3
 
-# include	<unistd.h>
-# include	<stdlib.h>
-# include	<string.h>
-# include	<fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
 
 int			ft_read_file(int fd, char **line, char **storage_pt);
 int			ft_has_newline(char **storage_pt, char *buf, char **line, int i);
